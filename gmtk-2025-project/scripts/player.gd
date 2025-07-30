3b50980146dda59161bbab3e5bd61dddaa0f316c
Cheese-code <cheesecode6@gmail.com>
2025-07-30 23:56:57 +0530

    added player

--- gmtk-2025-project/scripts/player.gd
+++ gmtk-2025-project/scripts/player.gd
@@ -2,18 +2,27 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+const jumping_gravity = 1500
+const falling_gravity = 3000
+const accelaration = 40
+const def_decel = 50
+
+var decel = def_decel
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		if velocity.y > 0:
+			velocity.y += falling_gravity * delta
+		else:
+			velocity.y += jumping_gravity*delta
 
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = move_toward(velocity.x,SPEED*direction,accelaration)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, decel)
 		
 	move_and_slide()
