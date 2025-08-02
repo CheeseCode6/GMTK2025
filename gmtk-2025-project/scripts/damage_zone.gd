22605ac0a5a6abd04e39e47f2f22b31632274ec7
Cheese-code <cheesecode6@gmail.com>
2025-08-02 12:12:10 +0530

    small changes to death block

--- gmtk-2025-project/scripts/damage_zone.gd
+++ gmtk-2025-project/scripts/damage_zone.gd
@@ -6,13 +6,16 @@ var plyr:Node2D
 func _on_body_entered(body: Node2D) -> void:
 	if body is CharacterBody2D:
 		plyr = body
-		body.place_block()
 		die()
 		
 func die()->void:
+	plyr.dead = true
+	plyr.velocity.y = 0
+	plyr.velocity.x = 0
 	Engine.time_scale = 0.5
 	death_wait.start()
 
 func _on_death_wait_timeout() -> void:
 	Engine.time_scale = 1
+	plyr.place_block()
 	plyr.respawn()
--- gmtk-2025-project/scripts/player.gd
+++ gmtk-2025-project/scripts/player.gd
@@ -4,13 +4,14 @@ extends CharacterBody2D
 @onready var jump_buffer: Timer = $jump_buffer
 @onready var coyote_time: Timer = $coyote_time
 
-const SPEED = 150
+const SPEED = 200
 const JUMP_VELOCITY = -300
-const jumping_gravity = 1700
-const falling_gravity = 2400
+const jumping_gravity = 1800
+const falling_gravity = 2200
 const accelaration = 80
 const def_decel = 80
 
+var dead = false
 var spawn_pos:Vector2
 var decel = def_decel
 var buffered = false
@@ -26,27 +27,29 @@ func _physics_process(delta: float) -> void:
 		if was_on_floor:
 			coyoted = true
 			coyote_time.start()
-		
+			
+	if not dead:
+		#gravity
 		if velocity.y > 0:
 			velocity.y += falling_gravity * delta
 		else:
 			velocity.y += jumping_gravity*delta
-
-	if Input.is_action_pressed("jump"):
-		if is_on_floor() or coyoted:
+				
+		#input
+		if Input.is_action_pressed("jump"):
+			if is_on_floor() or coyoted:
+				jump()
+			else:
+				buffered = true
+				jump_buffer.start()
+		if is_on_floor() and buffered:
 			jump()
+		var direction := Input.get_axis("left", "right")
+		if direction:
+			velocity.x = move_toward(velocity.x,SPEED*direction,accelaration)
 		else:
-			buffered = true
-			jump_buffer.start()
-	if is_on_floor() and buffered:
-		jump()
-	
-	var direction := Input.get_axis("left", "right")
-	if direction:
-		velocity.x = move_toward(velocity.x,SPEED*direction,accelaration)
-	else:
-		velocity.x = move_toward(velocity.x, 0, decel)
-		
+			velocity.x = move_toward(velocity.x, 0, decel)
+
 	was_on_floor = is_on_floor()
 	move_and_slide()
 
@@ -65,11 +68,12 @@ func place_block()->void:
 		last_block.queue_free()
 		last_block.visible = false
 	var block = preload("res://scenes/dead_blok.tscn").instantiate()
-	block.position.x = position.x
-	block.position.y = position.y
+	var offset := Vector2(0, -15)
+	block.global_position = global_position + offset
 	get_parent().call_deferred("add_child", block)
 	last_block = block
 	
 func respawn():
+	dead = false
 	position = spawn_pos
 	velocity = Vector2.ZERO
